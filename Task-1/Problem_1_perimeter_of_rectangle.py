40aaf0bdb2a6a2383c88137dfbb25f5e84017a09
Mohamed Ahmed Yousef <81932663+mohamedahmed-cloud@users.noreply.github.com>
2022-01-16 21:55:10 +0200

    Update Problem_1_perimeter_of_rectangle.py

--- Task-1/Problem_1_perimeter_of_rectangle.py
+++ Task-1/Problem_1_perimeter_of_rectangle.py
@@ -1,1 +1,7 @@
+#take inputs form user
+length_of_triangle=int(input("enter your length: "))
+width_of_triangle=int(input("enter your width: "))
+# display output 
+print(length_of_triangle*width_of_triangle)
+print((length_of_triangle+width_of_triangle)*2)
 
