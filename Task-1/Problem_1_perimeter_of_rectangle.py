329548abacaa6982c2fe6882ccebcf0d3dcfa3ec
Mohamed Ahmed Yousef <81932663+mohamedahmed-cloud@users.noreply.github.com>
2022-01-16 22:06:52 +0200

    Update Problem_1_perimeter_of_rectangle.py

--- Task-1/Problem_1_perimeter_of_rectangle.py
+++ Task-1/Problem_1_perimeter_of_rectangle.py
@@ -1,6 +1,6 @@
 #take inputs form user
-length_of_triangle=int(input("enter your length: "))
-width_of_triangle=int(input("enter your width: "))
+length_of_triangle=int(input("enter your length: "))  #only interger number for float number remove "int"====> float
+width_of_triangle=int(input("enter your width: "))    #only interger number for float number remove "int"====> float
 # display output 
 print(length_of_triangle*width_of_triangle)
 print((length_of_triangle+width_of_triangle)*2)
