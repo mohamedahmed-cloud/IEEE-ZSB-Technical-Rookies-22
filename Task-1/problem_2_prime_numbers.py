bfa7928ba3cdc460daaa43128d0a419f1057b702
Mohamed Ahmed Yousef <81932663+mohamedahmed-cloud@users.noreply.github.com>
2022-01-16 23:55:53 +0200

    Update problem_2_prime_numbers.py

--- Task-1/problem_2_prime_numbers.py
+++ Task-1/problem_2_prime_numbers.py
@@ -1,1 +1,21 @@
+# take user input
+user_number = int(input("enter your interger number: "))
+
+my_list=[] 
+for i in range(2,user_number+1):#number to test 
+    if user_number >=2:
+        my_list.append(2)
+    for j in range(2,i):#by this number "j"I can test
+        if  i%j==0 :
+            # my_list.remove(i)
+            break
+    else:
+        my_list.append(i)
+my_set=set(my_list)
+# display our expected output
+print(*my_set)
+# count number of prime number
+print(len(my_set))
+
+
 
