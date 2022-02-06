b70047597ac78265a65ecd25f02fda034deb03e4
Mohamed Ahmed Yousef <81932663+mohamedahmed-cloud@users.noreply.github.com>
2022-02-06 03:07:14 +0200

    Update problem_4_sentence_print_its_word-per_line_with_rectangular_frame.py

--- Task-1/problem_4_sentence_print_its_word-per_line_with_rectangular_frame.py
+++ Task-1/problem_4_sentence_print_its_word-per_line_with_rectangular_frame.py
@@ -1,1 +1,20 @@
+user_input = input("Enter your sentences ")
+user = user_input.split() #the varible here is very important ==> without it we will print each letter in new line
+# To bring the length of largest word
+list=[]
+for word in user:
+     list.append(len(word))
+my_longest_word=max(list)
+# print(my_longest_word)
 
+# To make the frame and display word
+w=1
+for word in user:
+    
+    if w==1:  #to prevent these stars from printing more one time 
+        print("*"*(my_longest_word+4))     # make up,down of frame is equal and equal the largest word length
+    w+=1             #to break the if in the next loop
+    print("*", end=" ")
+    print(word,end=" ")
+    print(" "*(my_longest_word-len(word))+"*") # To justify the left star
+print("*"*(my_longest_word+4))
