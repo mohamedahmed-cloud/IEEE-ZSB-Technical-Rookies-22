ee3cc5310ba0caeb806200ba6dd26101e819a38f
Mohamed Ahmed Yousef <81932663+mohamedahmed-cloud@users.noreply.github.com>
2022-02-07 01:44:24 +0200

    Update problem_4_Library Fine.py

--- Task_4/problem_4_Library Fine.py
+++ Task_4/problem_4_Library Fine.py
@@ -1,10 +1,10 @@
 # Write your code here
 if y1 > y2:
-    fine = 10000
+    fine = 10000 #given in the problem 
 elif m1 > m2 and y1 == y2:
-    fine = 500 * (m1 - m2)
+    fine = 500 * (m1 - m2) #it's given in the problem 
 elif d1 > d2 and m1 == m2 and y1 == y2:
-    fine = 15 * (d1 - d2)
+    fine = 15 * (d1 - d2) #given in the problem 
 else:
     fine=0   
 return fine
