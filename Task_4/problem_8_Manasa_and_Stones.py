e19e5770e83452a77b363e938f66c73d7b77696e
Mohamed Ahmed Yousef <81932663+mohamedahmed-cloud@users.noreply.github.com>
2022-02-07 15:27:44 +0200

    Update problem_8_Manasa_and_Stones.py

--- Task_4/problem_8_Manasa_and_Stones.py
+++ Task_4/problem_8_Manasa_and_Stones.py
@@ -1,1 +1,16 @@
-
+# Note that I remove the function in the editor to be able to use print built in funciton 
+# first of my code ################################################################
+t=int(input())
+for i in range(t):
+    n=int(input())
+    a=int(input())
+    b=int(input())
+    list=[]   
+    for j in range(n):
+        list.append((n-1)*min(a,b) + j*abs(a-b))
+    new_list=sorted(set(list))
+    for k in new_list:
+        print(k,end='')
+        print(" ",end='')
+    print(" ")
+# End of my code ###############################################
