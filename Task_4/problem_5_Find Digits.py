c8a2c5d61f850037ecd35ff2c7ef6b0069c6d0b0
Mohamed Ahmed Yousef <81932663+mohamedahmed-cloud@users.noreply.github.com>
2022-02-07 02:09:51 +0200

    Update problem_5_Find Digits.py

--- Task_4/problem_5_Find Digits.py
+++ Task_4/problem_5_Find Digits.py
@@ -1,1 +1,11 @@
-
+def findDigits(n):
+    # Write your code here
+    count=0
+    for i in range(t):
+        for j in str(n):
+            if int(j) ==0: #to prevent the operation of zero division 
+                pass
+            elif int(n) % int(j)==0:
+                count+=1
+        return count
+        count=0   #very important ==> to reset counter after finish the enter loop
