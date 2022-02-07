41b07b3f4ed903007f4a9c55c3289341042f859c
Mohamed Ahmed Yousef <81932663+mohamedahmed-cloud@users.noreply.github.com>
2022-02-07 16:14:42 +0200

    Update problem_8_Manasa_and_Stones.py

--- Task_4/problem_8_Manasa_and_Stones.py
+++ Task_4/problem_8_Manasa_and_Stones.py
@@ -1,16 +1,19 @@
-# Note that I remove the function in the editor to be able to use print built in funciton 
+# Note that I remove the function in the editor to be able to use print built in funciton
+
 # first of my code ################################################################
+# Our input ==>
 t=int(input())
 for i in range(t):
     n=int(input())
     a=int(input())
     b=int(input())
     list=[]   
+#     operation on input ==>output
     for j in range(n):
-        list.append((n-1)*min(a,b) + j*abs(a-b))
-    new_list=sorted(set(list))
-    for k in new_list:
+        list.append((n-1)*min(a,b) + j*abs(a-b))   #you can get it by mathmetheical operation and your notice ==> This relation is alitte diffcult
+    new_list=sorted(set(list))  #very important to make i sorted and unique value
+    for k in new_list:  #To display it with square brackets and ","
         print(k,end='')
         print(" ",end='')
-    print(" ")
+    print(" ")        # Make a new line after each operation
 # End of my code ###############################################
