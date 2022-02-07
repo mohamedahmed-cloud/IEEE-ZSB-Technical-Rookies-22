ed279220c0fee3744940671e1719cf70957d3de7
Mohamed Ahmed Yousef <81932663+mohamedahmed-cloud@users.noreply.github.com>
2022-02-07 03:26:18 +0200

    Update problem_5_Find Digits.py

--- Task_4/problem_5_Find Digits.py
+++ Task_4/problem_5_Find Digits.py
@@ -1,5 +1,21 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'findDigits' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER n as parameter.
+#
+
 def findDigits(n):
     # Write your code here
+#     my_code#######################################################
     count=0
     for i in range(t):
         for j in str(n):
@@ -9,3 +25,17 @@ def findDigits(n):
                 count+=1
         return count
         count=0   #very important ==> to reset counter after finish the enter loop
+# end of my code ##########################################################
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    t = int(input().strip())
+
+    for t_itr in range(t):
+        n = int(input().strip())
+
+        result = findDigits(n)
+
+        fptr.write(str(result) + '\n')
+
+    fptr.close()
