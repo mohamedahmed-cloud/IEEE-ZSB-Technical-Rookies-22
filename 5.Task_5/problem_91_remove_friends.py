c6326cc949e0e552b40c4d4a83511082eeaaf73b
Mohamed Ahmed Yousef <81932663+mohamedahmed-cloud@users.noreply.github.com>
2022-02-14 19:55:16 +0200

    All Solved

--- 5.Task_5/problem_91_remove_friends.py
+++ 5.Task_5/problem_91_remove_friends.py
@@ -1,1 +1,22 @@
-# This task doesn't reload 
+# input operation
+t=int(input())
+for i in range(t):
+    Number_of_friends,Wanted_deleted=list(map(int,input().split()))
+    Her_freinds=list(map(int,input().split()))
+   
+#    output operation
+    my_list = []                                                                  
+
+    for i in Her_freinds:                                                      
+
+       while Wanted_deleted and my_list and my_list[-1] < i:                      
+
+           my_list.pop()                                                          
+
+           Wanted_deleted -= 1                                                  
+
+       my_list.append(i)                                                    
+
+    print(' '.join(map(str, my_list)))
+  
+       
